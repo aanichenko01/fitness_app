22eb509bb2b9432afa169ae227de99139c24d3d9
aa03813 <aa03813@surrey.ac.uk>
2020-11-15 17:14:04 +0000

    Added basic testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
+
+    assert_select 'title', 'My Fitness'
+    assert_select 'h1', 'My Fitness'
+    assert_select 'p', 'Welcome to "My Fitness!"'
   end
 
 end
