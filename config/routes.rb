841ef3137766be7263ca6c80d1274931ef8d6c3c
aa03813 <aa03813@surrey.ac.uk>
2020-11-16 16:01:36 +0000

    Added basic form with minimal styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get 'home', to: "home#home"
 
   get 'contact', to: 'home#contact'
+
+  #post route for the contact form
+  post 'request_contact', to: 'home#request_contact'
+
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -10,4 +10,13 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_select 'p', 'Welcome to "Health Me!"'
   end
 
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+    assert_template layout: 'application'
+    assert_select 'title', 'Health Me'
+    assert_select 'h1', 'Contact Us'
+    assert_select 'p', 'Fill in the form below to contact us.'
+  end
+
 end
