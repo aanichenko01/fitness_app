cce04e6ec10afe761faff0d9786518161951c6f5
aa03813 <aa03813@surrey.ac.uk>
2020-11-17 12:05:57 +0000

    Small fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,26 +19,19 @@
 //= require_tree .
 
 function ValidateForm(){
-    // Initialize form validation on the registration form.
-    // It has the name attribute "registration"
     $("form").validate({
-        // Specify validation rules
+        //Validation rules
         rules: {
-          // The key name on the left side is the name attribute
-          // of an input field. Validation rules are defined
-          // on the right side
           name: 'required',
           email: 'required',
           message: 'required'
         },
-        // Specify validation error messages
+        //Validation error messages
         messages: {
           name: 'Please enter your firstname',
           email: 'Please enter a valid email address',
           message: 'Please enter a message'
         },
-        // Make sure the form is submitted to the destination defined
-        // in the "action" attribute of the form when valid
         submitHandler: function(form) {
           form.submit();
         }
@@ -46,8 +39,6 @@ function ValidateForm(){
 }
 
 $(document).ready(function() {
-    
       ValidateForm();
-  
-  });
+});
   
