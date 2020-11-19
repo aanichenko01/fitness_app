9a1b6feae9162bffea86831d0d885ec35c723804
aa03813 <aa03813@surrey.ac.uk>
2020-11-19 11:26:25 +0000

    Made rows selectable(pathing does not work)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,7 +38,16 @@ function ValidateForm(){
       });
 }
 
+function SelectableRows(){
+  $(document).on('dblclick', 'td', function(){
+    var page_url = 'localhost:3000/workouts/' + this.className;
+    alert(page_url);
+  });
+}
+
+
 $(document).ready(function() {
       ValidateForm();
+      SelectableRows()
 });
   
