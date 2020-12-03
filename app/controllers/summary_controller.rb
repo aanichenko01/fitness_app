963933dc4b3af510b039d9a57b310197cbcee662
aa03813 <aa03813@surrey.ac.uk>
2020-12-03 15:18:41 +0000

    Removed unecessary logic from view

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -2,5 +2,22 @@ class SummaryController < ApplicationController
   before_action :authenticate_user!
   
   def summary
+
+    @totalCalories = 0
+    @totalDuration = 0
+    @totalHours = 0
+    @totalMins = 0 
+    # calculates total calories for this month
+    helpers.workouts_this_month.each do |workout|
+      @totalCalories += workout.calories
+    end
+    # calculates total duration of workouts for this month in hours and minutes
+    helpers.workouts_this_month.each do |workout|
+      @totalDuration += workout.duration
+    end
+    @totalHours = @totalDuration/60
+    @totalMins = @totalDuration % 60
   end
+
+  
 end
--- app/helpers/summary_helper.rb
+++ app/helpers/summary_helper.rb
@@ -1,2 +1,15 @@
 module SummaryHelper
+    #helper method to display weight difference
+    def weight_difference
+        if current_user
+          return number_with_precision(Weight.weight_difference(current_user), :precision => 1)
+          return string.html_safe
+        end
+    end
+
+    #helper method to return workouts for current month
+    def workouts_this_month
+      return Workout.this_month(current_user)
+      return string.html_safe
+    end
 end
