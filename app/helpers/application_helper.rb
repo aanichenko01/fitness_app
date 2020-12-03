fa1447a2221c5f869df317096711be91b2bf94b2
aa03813 <aa03813@surrey.ac.uk>
2020-12-03 11:09:58 +0000

    Styled New Workout form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
     #helper method to display weight difference
     def weight_difference
         if current_user
-          return number_with_precision(Weight.weight_difference(current_user), :precision => 2)
+          return number_with_precision(Weight.weight_difference(current_user), :precision => 1)
           return string.html_safe
         end
     end
 
-    #helper method to display calories burned
+    #helper method to return workouts for current month
     def workouts_this_month
       return Workout.this_month(current_user)
       return string.html_safe
