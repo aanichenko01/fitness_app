21a5d711dea922768c7693a5564b88c6a33fdddc
aa03813 <aa03813@surrey.ac.uk>
2020-12-07 14:33:02 +0000

    Fixed totalCalories error

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -2,23 +2,23 @@ class SummaryController < ApplicationController
   before_action :authenticate_user!
   
   def summary
-
-    if current_user.workouts.first.present?
-      # calculates total calories for this month
-      helpers.workouts_this_month.each do |workout|
-        @totalCalories += workout.calories
-      end
-      # calculates total duration of workouts for this month in hours and minutes
-      helpers.workouts_this_month.each do |workout|
-        @totalDuration += workout.duration
-      end
-      @totalHours = @totalDuration/60
-      @totalMins = @totalDuration%60
+    @totalCalories = 0
+    @totalDuration = 0
+    @totalHours = 0
+    @totalMins = 0 
+    # calculates total calories for this month
+    helpers.workouts_this_month.each do |workout|
+      @totalCalories += workout.calories
     end
+    # calculates total duration of workouts for this month in hours and minutes
+    helpers.workouts_this_month.each do |workout|
+      @totalDuration += workout.duration
+    end
+    @totalHours = @totalDuration/60
+    @totalMins = @totalDuration%60
 
     if current_user.weights.first.present?
       @weightDifference = helpers.weights_this_month.first.weight - helpers.weights_this_month.last.weight
-
       if @weightDifference > 0
         #makes weight appear as a negative to show weight loss
         @weightDifference = @weightDifference * -1
