26ecc97567d6ad700fb98bbfa842ec36d1a8c157
aa03813 <aa03813@surrey.ac.uk>
2020-12-09 16:08:17 +0000

    Added more weight model tests

--- test/models/weight_test.rb
+++ test/models/weight_test.rb
@@ -23,4 +23,71 @@ class WeightTest < ActiveSupport::TestCase
     weight.save
     assert weight.valid?
   end
+
+  test 'should get earliest date entry' do
+
+    weightOne = Weight.new
+    weightOne.date = '01-12-2020'
+    weightOne.weight = 100.2
+    weightOne.user = @user
+    weightOne.save
+
+    weightTwo = Weight.new
+    weightTwo.date = '14-12-2020'
+    weightTwo.weight = 80.2
+    weightTwo.user = @user
+    weightTwo.save
+
+    assert_equal(weightOne, Weight.first_weight(@user))
+  end 
+
+  test 'should get latest date entry' do
+
+    weightOne = Weight.new
+    weightOne.date = '01-12-2020'
+    weightOne.weight = 100.2
+    weightOne.user = @user
+    weightOne.save
+
+    weightTwo = Weight.new
+    weightTwo.date = '14-12-2020'
+    weightTwo.weight = 80.2
+    weightTwo.user = @user
+    weightTwo.save
+
+    assert_equal(weightTwo, Weight.current_weight(@user))
+  end 
+
+  test 'should get weight loss' do
+    weightOne = Weight.new
+    weightOne.date = '01-12-2020'
+    weightOne.weight = 85.2
+    weightOne.user = @user
+    weightOne.save
+
+    weightTwo = Weight.new
+    weightTwo.date = '14-12-2020'
+    weightTwo.weight = 80.2
+    weightTwo.user = @user
+    weightTwo.save
+
+    assert_equal(-5.0, Weight.weight_difference(@user))
+  end 
+
+  test 'should get weight gain' do
+    weightOne = Weight.new
+    weightOne.date = '01-12-2020'
+    weightOne.weight = 80.2
+    weightOne.user = @user
+    weightOne.save
+
+    weightTwo = Weight.new
+    weightTwo.date = '14-12-2020'
+    weightTwo.weight = 85.2
+    weightTwo.user = @user
+    weightTwo.save
+
+    assert_equal(5.0, Weight.weight_difference(@user))
+  end
+  
 end
