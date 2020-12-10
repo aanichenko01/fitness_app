3d9de4224f9d32e02fa37a3bdbd12c94a8657912
aa03813 <aa03813@surrey.ac.uk>
2020-12-10 19:10:38 +0000

    Made weight model tests DRY

--- test/models/weight_test.rb
+++ test/models/weight_test.rb
@@ -3,8 +3,21 @@ require 'test_helper'
 class WeightTest < ActiveSupport::TestCase
   
   #Creates a user to associate weight to
+  #Creates two weight variables to test weight model methods
   setup do
     @user = users(:one)
+
+    @weightOne = Weight.new
+    @weightOne.date = '01-11-2020'
+    @weightOne.weight = 85.2
+    @weightOne.user = @user
+    @weightOne.save
+
+    @weightTwo = Weight.new
+    @weightTwo.date = '14-01-2021'
+    @weightTwo.weight = 80.2
+    @weightTwo.user = @user
+    @weightTwo.save
   end
   
   test 'should not save empty weight' do
@@ -26,69 +39,22 @@ class WeightTest < ActiveSupport::TestCase
   end
 
   test 'should get earliest date entry' do
-
-    weightOne = Weight.new
-    weightOne.date = '01-12-2020'
-    weightOne.weight = 100.2
-    weightOne.user = @user
-    weightOne.save
-
-    weightTwo = Weight.new
-    weightTwo.date = '14-12-2020'
-    weightTwo.weight = 80.2
-    weightTwo.user = @user
-    weightTwo.save
-
-    assert_equal(weightOne, Weight.first_weight(@user))
+    assert_equal(@weightOne, Weight.first_weight(@user))
   end 
 
   test 'should get latest date entry' do
-
-    weightOne = Weight.new
-    weightOne.date = '01-12-2020'
-    weightOne.weight = 100.2
-    weightOne.user = @user
-    weightOne.save
-
-    weightTwo = Weight.new
-    weightTwo.date = '14-12-2020'
-    weightTwo.weight = 80.2
-    weightTwo.user = @user
-    weightTwo.save
-
-    assert_equal(weightTwo, Weight.current_weight(@user))
+    assert_equal(@weightTwo, Weight.current_weight(@user))
   end 
 
   test 'should get weight loss' do
-    weightOne = Weight.new
-    weightOne.date = '01-12-2020'
-    weightOne.weight = 85.2
-    weightOne.user = @user
-    weightOne.save
-
-    weightTwo = Weight.new
-    weightTwo.date = '14-12-2020'
-    weightTwo.weight = 80.2
-    weightTwo.user = @user
-    weightTwo.save
-
     assert_equal(-5.0, Weight.weight_difference(@user))
   end 
 
   test 'should get weight gain' do
-    weightOne = Weight.new
-    weightOne.date = '01-12-2020'
-    weightOne.weight = 80.2
-    weightOne.user = @user
-    weightOne.save
-
-    weightTwo = Weight.new
-    weightTwo.date = '14-12-2020'
-    weightTwo.weight = 85.2
-    weightTwo.user = @user
-    weightTwo.save
-
+    @weightOne.weight = 80.2
+    @weightOne.save
+    @weightTwo.weight = 85.2
+    @weightTwo.save
     assert_equal(5.0, Weight.weight_difference(@user))
   end
-  
 end
